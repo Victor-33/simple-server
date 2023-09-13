9c603eabe93352178206a0e4c310703e001ed8f3
Victor <vi_bgd@hotmail.com>
2023-09-13 19:20:43 -0300

    axios

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const http = require('http');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
+const axios = require('axios');
+
+async function keepServerActive() {
+  try {
+    // Substitua 'http://seusite.com' pela URL do seu servidor
+    const response = await axios.get('http://seusite.com');
+    console.log('Solicitação bem-sucedida:', response.status);
+  } catch (error) {
+    console.error('Erro na solicitação:', error.message);
+  }
+}
+
+// Faça a primeira solicitação assim que o servidor for iniciado
+keepServerActive();
+
+// Agende solicitações periódicas (por exemplo, a cada 5 minutos)
+setInterval(keepServerActive, 5 * 60 * 1000); // 5 minutos
+
 
 const server = http.createServer((req, res) => {
   fs.readFile('index.html', (err, data) => {
